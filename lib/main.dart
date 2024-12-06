fc5479f656ebcb2afef0c6ee7381f95e4c544147
su18nny <112651949+su18nny@users.noreply.github.com>
2024-12-06 15:07:25 +0530

    splashscreendone
    done

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,9 @@ class SplashScreen extends StatelessWidget {
               height: screenHeight * 0.7,
               decoration: BoxDecoration(
                 color: Colors.white,
+
                 image: DecorationImage(
                   image: AssetImage("assets/images/Group 1436.jpg"),
-
                   fit: BoxFit.fitWidth
                 ),
               ),
@@ -39,29 +39,28 @@ class SplashScreen extends StatelessWidget {
           Column(
             children: [
               Container(
+
                 width: screenWidth*0.8,
                 height:screenHeight*0.1,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   image: DecorationImage(
                     image: AssetImage("assets/images/Layer_1.jpg"),
+
                     scale: 0.10,
                   ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 30),
+                padding: const EdgeInsets.only(top: 38),
                 child: SizedBox(height: 52,width: 320,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Text(
-                      "Get Started",
-                      style: TextStyle(color: Colors.white,fontSize: 20),
-                    ),
-                    style: IconButton.styleFrom(backgroundColor: Colors.blue, ),
-                  ),
+                  child: ElevatedButton(onPressed: () {
 
+                  }, child:Text("Get Started",style: TextStyle(color: Colors.white,fontSize: 20,letterSpacing: 4),),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.black87,shadowColor: Colors.white,elevation: 10),
+                  ),
                 ),
+
               ),
 
             ],
