6896648d1551c67c2c67fe999486f8338dbe189c
su18nny <112651949+su18nny@users.noreply.github.com>
2024-12-06 12:41:23 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/real_estateui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.real_estateui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+            Container(
+              width: screenWidth * 1,
+              height: screenHeight * 0.7,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/Group 1436.jpg"),
+
+                  fit: BoxFit.fitWidth
+                ),
+              ),
+            ),
+          Column(
+            children: [
+              Container(
+                width: screenWidth*0.8,
+                height:screenHeight*0.1,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/Layer_1.jpg"),
+                    scale: 0.10,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: SizedBox(height: 52,width: 320,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Text(
+                      "Get Started",
+                      style: TextStyle(color: Colors.white,fontSize: 20),
+                    ),
+                    style: IconButton.styleFrom(backgroundColor: Colors.blue, ),
+                  ),
+
+                ),
+              ),
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
